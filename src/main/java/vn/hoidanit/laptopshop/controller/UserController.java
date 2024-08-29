08898b45ca75bd6da5c8d8b0c3832d6ef49e56b2
Hoang-Minh-Tri <minhtri4869it@gmail.com>
2024-08-29 10:38:03 +0700

    Hello

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,11 +1,13 @@
 package vn.hoidanit.laptopshop.controller;
 
+import org.eclipse.tags.shaded.org.apache.xpath.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.core.model.Model;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +22,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
-        return "eric.html";
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute(test, test);
+        return "hello";
     }
 }
 
